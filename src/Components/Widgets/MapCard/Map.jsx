93eaaa202b4934fbedfd06b205486b347cfbd99f
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-02-09 21:08:40 +0530

    feat(Widgets/MapCard)

---
+++ src/Components/Widgets/MapCard/Map.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Map() {
+  return (
+    <div className="flex flex-col border w-full h-full rounded-[10px] bg-white shadow"></div>
+  );
+}
+
+export default Map;
