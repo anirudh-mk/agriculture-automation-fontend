8b7e89a6572a869d62800181560255ff6a3bdab6
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-02-09 15:37:58 +0530

    feat(dashboard)

--- src/Components/Screens/Dashboard/Dashboard.jsx
+++ src/Components/Screens/Dashboard/Dashboard.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <>
+      <div className="flex h-screen w-screen">
+        <div className="w-[300px] h-full px-[10px] py-[10px]">
+          <div className="border w-full h-full rounded-[10px] bg-white shadow">
+            <h1 className="text-2xl font-bold px-[20px] py-[20px] text-[#076DFC]">
+              Dashboard
+            </h1>
+          </div>
+        </div>
+        <div className="flex-1 h-screen px-[10px] py-[10px]">
+          <div className="border w-full h-full rounded-[10px] bg-white shadow"></div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Dashboard;
