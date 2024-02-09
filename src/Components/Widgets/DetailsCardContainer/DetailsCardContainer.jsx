391e3d37696030cd20441a7c0090fd95301ed7e7
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-02-09 16:55:17 +0530

    feat(dashboard)

--- src/Components/Widgets/DetailsCardContainer/DetailsCardContainer.jsx
+++ src/Components/Widgets/DetailsCardContainer/DetailsCardContainer.jsx
@@ -2,15 +2,17 @@ import React from "react";
 
 function DetailsCardContainer() {
   return (
-    <div className="w-full px-[40px] py-[20px] border shadow-md bg-white rounded-lg">
+    <div className="w-full py-[20px] border shadow-md bg-white rounded-lg">
       <div className="text-xl font-semibold">
-        <div className="flex justify-between">
-          <p>1</p>
-          <p>Anirudh MK</p>
-          <p>farm 1</p>
-          <p>kerala</p>
-          <img src="" alt="" className="w-[30px] h-[30px] border" />
-          <div className="w-[20px] h-[20px]"></div>
+        <div className="grid grid-cols-5 ">
+          <p className="px-[60px]">1</p>
+          <p className="px-[10px]">Anirudh MK</p>
+          <p className="px-[10px]">farm 1</p>
+          <p className="px-[10px]">kerala</p>
+          <div className="flex px-[10px] justify-between">
+            <img src="" alt="" className="w-[30px] h-[30px] border" />
+            <div className="w-[20px] h-[20px]"></div>
+          </div>
         </div>
       </div>
     </div>
--- src/Components/Widgets/UserCard/UserCard.jsx
+++ src/Components/Widgets/UserCard/UserCard.jsx
@@ -4,8 +4,8 @@ import DetailsCardContainer from "../DetailsCardContainer/DetailsCardContainer";
 function UserCard() {
   return (
     <div className="flex flex-col border w-full h-full rounded-[10px] bg-white shadow">
-      <div className="flex justify-between items-center w-full px-[20px] py-[20px]">
-        <h1 className="text-2xl font-bold text-[#076DFC]">User</h1>
+      <div className="flex justify-between items-center w-full px-[40px] py-[40px]">
+        <h1 className="text-3xl font-bold text-[#076DFC]">User</h1>
         <button
           type="button"
           class="px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
@@ -13,7 +13,18 @@ function UserCard() {
           Extra small
         </button>
       </div>
-      <div className="w-full px-[20px] py-[40px]">
+      <div className="px-[40px] py-[40px]">
+        <div className="w-full py-[20px] ">
+          <div className="text-xl font-semibold">
+            <div className="grid grid-cols-5 items-center">
+              <p className="px-[40px]">Sl.No</p>
+              <p className="px-[10px]">Username</p>
+              <p className="px-[10px]">Farm Name</p>
+              <p className="px-[10px]">Location</p>
+              <input type="text" className="w-full h-[40px] rounded-[40px]" />
+            </div>
+          </div>
+        </div>
         <DetailsCardContainer />
       </div>
     </div>
