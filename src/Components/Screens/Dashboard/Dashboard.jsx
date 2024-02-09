5e13e63bcb620e441a4a33c2fc0be50232de0d12
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-02-09 17:36:57 +0530

    feat(Dashboard)

--- src/Components/Screens/Dashboard/Dashboard.jsx
+++ src/Components/Screens/Dashboard/Dashboard.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import UserCard from "../../Widgets/UserCard/UserCard";
-
+import IconButton from "../../Widgets/IconButton/IconButton";
+import { faUsers } from "@fortawesome/free-solid-svg-icons";
+import { faPepperHot } from "@fortawesome/free-solid-svg-icons";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import { faMessage } from "@fortawesome/free-solid-svg-icons";
+import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 function Dashboard() {
   return (
     <>
@@ -8,9 +13,56 @@ function Dashboard() {
         <div className="w-[300px] h-full px-[10px] py-[10px]">
           <div className="w-full h-full rounded-[10px] bg-white shadow border px-[20px] py-[40px]">
             <h1 className="text-3xl font-bold text-[#076DFC]">Dashboard</h1>
-            <div className="flex flex-col">
-              <div></div>
-              <div></div>
+            <div className="flex flex-col justify-between h-full">
+              <div className="py-[40px]">
+                <div className="py-[10px]">
+                  <IconButton
+                    height={50}
+                    backgroundColor="#076DFC"
+                    borderRadius={10}
+                    buttonText="User"
+                    icon={faUsers}
+                  />
+                </div>
+                <div className="py-[10px]">
+                  <IconButton
+                    height={50}
+                    backgroundColor="#076DFC"
+                    borderRadius={10}
+                    buttonText="Vegetables"
+                    icon={faPepperHot}
+                  />
+                </div>
+                <div className="py-[10px]">
+                  <IconButton
+                    height={50}
+                    backgroundColor="#076DFC"
+                    borderRadius={10}
+                    buttonText="Map"
+                    icon={faLocationDot}
+                  />
+                </div>
+              </div>
+              <div className="py-[40px]">
+                <div className="py-[10px]">
+                  <IconButton
+                    height={50}
+                    backgroundColor="#076DFC"
+                    borderRadius={10}
+                    buttonText="Messages"
+                    icon={faMessage}
+                  />
+                </div>
+                <div className="py-[10px]">
+                  <IconButton
+                    height={50}
+                    backgroundColor="tomato"
+                    borderRadius={10}
+                    buttonText="Logout"
+                    icon={faRightFromBracket}
+                  />
+                </div>
+              </div>
             </div>
           </div>
         </div>
