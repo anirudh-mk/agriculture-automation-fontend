00b7a834a391a075d219f9538fb2f2c3c22d6ec4
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-02-09 17:00:46 +0530

    feat(IconButton and Text Button)

---
+++ src/Components/Widgets/IconButton/IconButton.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./IconButton.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+function IconButton({
+  height,
+  width,
+  borderRadius,
+  backgroundColor,
+  buttonText,
+  color,
+  icon,
+  onClick,
+}) {
+  return (
+    <div
+      className="iconButtonContainer"
+      style={{
+        width: width ? width : "100%",
+        height: height ? height : 40,
+        borderRadius: borderRadius ? borderRadius : 10,
+        backgroundColor: backgroundColor ? backgroundColor : "#076DFC",
+      }}
+      onClick={onClick}
+    >
+      <div className="iconButtonIconContainer" style={{ width: height }}>
+        <FontAwesomeIcon
+          icon={icon}
+          size="lg"
+          style={{ color: color ? color : "#fff" }}
+        />
+      </div>
+      <div className="iconButtonTextContainer">
+        <p style={{ color: color ? color : "#fff" }}>{buttonText}</p>
+      </div>
+    </div>
+  );
+}
+
+export default IconButton;
