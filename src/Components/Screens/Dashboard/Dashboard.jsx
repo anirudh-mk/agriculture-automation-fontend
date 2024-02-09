fca4654267068c8f8059628e545bde857a6c15e4
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-02-09 17:09:19 +0530

    feat(UserCard):Create button added

--- src/Components/Screens/Dashboard/Dashboard.jsx
+++ src/Components/Screens/Dashboard/Dashboard.jsx
@@ -8,6 +8,10 @@ function Dashboard() {
         <div className="w-[300px] h-full px-[10px] py-[10px]">
           <div className="w-full h-full rounded-[10px] bg-white shadow border px-[20px] py-[40px]">
             <h1 className="text-3xl font-bold text-[#076DFC]">Dashboard</h1>
+            <div className="flex flex-col">
+              <div></div>
+              <div></div>
+            </div>
           </div>
         </div>
         <div className="flex-1 h-screen px-[10px] py-[10px]">
--- src/Components/Widgets/UserCard/UserCard.jsx
+++ src/Components/Widgets/UserCard/UserCard.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import DetailsCardContainer from "../DetailsCardContainer/DetailsCardContainer";
+import IconButton from "../IconButton/IconButton";
+import { faCirclePlus } from "@fortawesome/free-solid-svg-icons";
 
 function UserCard() {
   return (
     <div className="flex flex-col border w-full h-full rounded-[10px] bg-white shadow">
       <div className="flex justify-between items-center w-full px-[40px] py-[40px]">
         <h1 className="text-3xl font-bold text-[#076DFC]">User</h1>
-        <button
-          type="button"
-          class="px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          Extra small
-        </button>
+        <IconButton
+          width={120}
+          height={40}
+          buttonText="create"
+          icon={faCirclePlus}
+        />
       </div>
       <div className="px-[40px] py-[40px]">
         <div className="w-full py-[20px] ">
