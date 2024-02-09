0741856af6b82902c5a45a6f3583616b7a17404e
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-02-09 15:57:37 +0530

    feat(User)

--- src/Components/Screens/Dashboard/Dashboard.jsx
+++ src/Components/Screens/Dashboard/Dashboard.jsx
@@ -1,18 +1,17 @@
 import React from "react";
+import UserCard from "../../Widgets/UserCard/UserCard";
 
 function Dashboard() {
   return (
     <>
       <div className="flex h-screen w-screen">
         <div className="w-[300px] h-full px-[10px] py-[10px]">
-          <div className="border w-full h-full rounded-[10px] bg-white shadow">
-            <h1 className="text-2xl font-bold px-[20px] py-[20px] text-[#076DFC]">
-              Dashboard
-            </h1>
+          <div className="border w-full h-full rounded-[10px] bg-white shadow px-[20px] py-[20px]">
+            <h1 className="text-2xl font-bold text-[#076DFC]">Dashboard</h1>
           </div>
         </div>
         <div className="flex-1 h-screen px-[10px] py-[10px]">
-          <div className="border w-full h-full rounded-[10px] bg-white shadow"></div>
+          <UserCard />
         </div>
       </div>
     </>
---
+++ src/Components/Widgets/UserCard/UserCard.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function UserCard() {
+  return (
+    <div className="flex flex-col border w-full h-full rounded-[10px] bg-white shadow">
+      <div className="flex justify-between items-center w-full px-[20px] py-[20px]">
+        <h1 className="text-2xl font-bold text-[#076DFC]">User</h1>
+        <button
+          type="button"
+          class="px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Extra small
+        </button>
+      </div>
+      <div className="w-full px-[20px] py-[40px]"></div>
+    </div>
+  );
+}
+
+export default UserCard;
