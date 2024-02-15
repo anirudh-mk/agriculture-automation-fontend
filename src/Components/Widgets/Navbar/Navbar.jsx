26a9a563ffb8119f7321684a00559fa0c21c3118
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-02-15 18:29:11 +0530

    feat(sidebar added)

--- src/Components/Widgets/Navbar/Navbar.jsx
+++ src/Components/Widgets/Navbar/Navbar.jsx
@@ -1,15 +1,38 @@
-import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faClose } from "@fortawesome/free-solid-svg-icons";
+import React, { useState } from "react";
+import "./Navbar.css";
 
 function Navbar() {
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+  const openSidebar = () => {
+    setSidebarOpen(true);
+  };
+  const closeSlidebar = () => {
+    setSidebarOpen(false);
+  };
   return (
-    <div className="fixed border-b z-50 bg-white">
+    <div className="fixed border-b z-40 bg-white">
       <div className="flex items-center w-screen p-[16px] justify-between">
-        <h1 className="text-2xl font-bold">Farmz</h1>
+        <div className="flex items-center">
+          <FontAwesomeIcon icon={faBars} size="lg" onClick={openSidebar} />
+          <h1 className="text-2xl font-bold px-[10px]">Farmz</h1>
+        </div>
         <img
           src="https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250"
           alt=""
           className="w-[40px] h-[40px] rounded-[40px]"
         />
+      </div>
+      <div
+        className={`bg-white h-full fixed top-0 w-[250px] left-0 z-50 border-r sidebarx ${
+          isSidebarOpen ? "open" : ""
+        }`}
+      >
+        <div className="flex w-full justify-end p-[16px]">
+          <FontAwesomeIcon icon={faClose} size="xl" onClick={closeSlidebar} />
+        </div>
       </div>
     </div>
   );
