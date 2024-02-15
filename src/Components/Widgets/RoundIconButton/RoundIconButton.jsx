d60ef4565c5f9774006879b872278e1d60b65360
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-02-15 15:43:56 +0530

    feat(src\Components\Widgets\RoundIconButton )

--- src/Components/Widgets/RoundIconButton/RoundIconButton.jsx
+++ src/Components/Widgets/RoundIconButton/RoundIconButton.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
-function RoundIconButton() {
+function RoundIconButton({ onClick }) {
   return (
-    <div className="fixed bottom-[24px] right-[24px] w-[50px] h-[50px] bg-[tomato] rounded-[25px] flex justify-center items-center">
+    <div
+      onClick={onClick}
+      className="fixed bottom-[24px] right-[24px] w-[50px] h-[50px] bg-[tomato] rounded-[25px] flex justify-center items-center"
+    >
       <FontAwesomeIcon icon={faPlus} size="xl" color="white"></FontAwesomeIcon>
     </div>
   );
