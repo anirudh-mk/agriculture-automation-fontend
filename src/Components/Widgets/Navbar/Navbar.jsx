679f502dfa486ed5382abd7359cb7b9ae0b011fe
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-02-20 23:07:32 +0530

    feat(src\Components\Widgets\Navbar):Navbar

--- src/Components/Widgets/Navbar/Navbar.jsx
+++ src/Components/Widgets/Navbar/Navbar.jsx
@@ -17,47 +17,43 @@ function Navbar({ handleUserClick, handleVegetableClick, handleMapClick }) {
     setSidebarOpen(false);
   };
   return (
-    <div className="fixed border-b z-40 bg-white">
-      <div className="flex items-center w-screen p-[16px] justify-between">
-        <div className="flex items-center">
-          <FontAwesomeIcon icon={faBars} size="lg" onClick={openSidebar} />
-          <h1 className="text-2xl font-bold px-[10px]">Farmz</h1>
+    <div className="navbar">
+      <div className="headdings-items">
+        <div className="headding">
+          <FontAwesomeIcon
+            icon={faBars}
+            size="lg"
+            onClick={openSidebar}
+            className="toggle-icon"
+          />
+          <h1>Farmz</h1>
         </div>
         <img
           src="https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250"
           alt=""
-          className="w-[40px] h-[40px] rounded-[40px]"
         />
       </div>
-      <div
-        className={`bg-white h-full fixed top-0 w-[250px] left-0 z-50 border-r sidebarx ${
-          isSidebarOpen ? "open" : ""
-        }`}
-      >
-        <div className="flex w-full justify-end p-[16px]">
-          <FontAwesomeIcon icon={faClose} size="xl" onClick={closeSlidebar} />
+      <div className={`sidebar ${isSidebarOpen ? "open" : ""}`}>
+        <div className="items">
+          <FontAwesomeIcon
+            icon={faClose}
+            size="xl"
+            onClick={closeSlidebar}
+            className="close-button"
+          />
         </div>
-        <div className="px-[16px] py-[16px]">
-          <div
-            className="flex items-center px-[10px] py-[10px] rounded-[10px] animation my-[5px]"
-            onClick={handleUserClick}
-          >
+        <div className="inner-container">
+          <div className="button" onClick={handleUserClick}>
             <FontAwesomeIcon icon={faUser} size="lg" />
-            <p className=" text-[20px] font-semibold px-[20px]">User</p>
+            <p>User</p>
           </div>
-          <div
-            className="flex items-center px-[10px] py-[10px] rounded-[10px] animation my-[5px]"
-            onClick={handleVegetableClick}
-          >
+          <div className="button" onClick={handleVegetableClick}>
             <FontAwesomeIcon icon={faPepperHot} size="lg" />
-            <p className=" text-[20px] font-semibold px-[20px]">Vegetabales</p>
+            <p>Vegetabales</p>
           </div>
-          <div
-            className="flex items-center px-[10px] py-[10px] rounded-[10px] animation my-[5px]"
-            onClick={handleMapClick}
-          >
+          <div className="button" onClick={handleMapClick}>
             <FontAwesomeIcon icon={faLocationDot} size="lg" />
-            <p className=" text-[20px] font-semibold px-[20px]">Map</p>
+            <p>Map</p>
           </div>
         </div>
       </div>
