d77cd390a7d3b08296fdecf3383ef3ea134ff623
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-02-21 22:11:38 +0530

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
 import "./App.css";
 import Dashboard from "./Components/Screens/Dashboard/Dashboard";
+import Login from "./Components/Screens/Login/Login";
 
 function App() {
+  const [login, setLogin] = useState(false);
+  const handleLogin = () => {
+    setLogin(true);
+  };
+  const handleLogoutClick = () => {
+    setLogin(false);
+  };
   return (
     <>
-      <Dashboard />
+      {login ? (
+        <Dashboard handleLogoutClick={handleLogoutClick} />
+      ) : (
+        <Login onClick={handleLogin} />
+      )}
     </>
   );
 }
--- src/Components/Widgets/MapCard/MapCard.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function MapCard() {
-  return (
-    <div className="flex flex-col border w-full h-full rounded-[10px] bg-white shadow"></div>
-  );
-}
-
-export default MapCard;
